8d6b30692fe3a1d1da1106be6b6b24cf67825f89
Jim O'Donnell <jim@zooniverse.org>
2021-09-09 09:11:25 +0100

    Add modes Choose between a fast, bright mode and a slow, minor mode.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const chromaticOctave = ["C4", 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab5', 'A5', 'Bb5', 'B5', 'C5'];
+const minorOctave = ["C4", 'D4', 'Eb4', 'F4', 'G4', 'Ab5', 'Bb5', 'C5'];
 
 const button = document.getElementById('play');
+const radios = document.querySelectorAll('input[type=radio]')
+
+let useMinorScale = false
+
+radios.forEach(radio => {
+  radio.addEventListener('change', onModeChange);
+  if (radio.checked) {
+    useMinorScale = radio.value === 'minor';
+  }
+})
 
 const APIOptions = {
   headers: {
@@ -22,27 +33,37 @@ const yMax = Math.max(...y)
 const yDiff = yMax - yMin
 console.log({ yMin, yMax })
 
+function onModeChange(event) {
+  if (event.target.checked) {
+    useMinorScale = event.target.value === 'minor';
+  }
+}
+
 function playData(xValue, index, synth, now) {
   const yValue = y[index]
-  const toneIndex = parseInt((yValue - yMin) * (55 / yDiff))
-  const start = now + xValue
-  console.log({ yValue, toneIndex, start })
-  const tone = chromaticOctave[toneIndex]
-  synth.triggerAttackRelease(tone, "8n", start)
+  const tonesArray = useMinorScale ? minorOctave : chromaticOctave;
+  const scaleFactor = tonesArray.length * 5;
+  const duration = useMinorScale ? "4n": "8n";
+  const toneIndex = parseInt((yValue - yMin) * (scaleFactor / yDiff));
+  const interval = useMinorScale ? xValue * 2 : xValue;
+  const start = now + interval;
+  console.log({ yValue, toneIndex, start, duration })
+  const tone = tonesArray[toneIndex]
+  synth.triggerAttackRelease(tone, duration, start)
 }
 
 async function playScale() {
   await Tone.start()
   const synth = new Tone.Synth().toDestination();
   const now = Tone.now()
-  for (let index = 0; index < 12; index++) {
-    synth.triggerAttackRelease(chromaticOctave[11 - index], "8n", now + (index * 0.1))
+  const tonesArray = useMinorScale ? minorOctave : chromaticOctave;
+  for (let index = 0; index < tonesArray.length; index++) {
+    synth.triggerAttackRelease(tonesArray[11 - index], "8n", now + (index * 0.1))
   }
   const trimmedData = x.filter((x, index) => {
     const remainder = index % 20;
     return remainder === 0;
   });
-  console.log(trimmedData.length)
   trimmedData.forEach((xValue, index) => playData(xValue, index, synth, now + 2))
 }
 
